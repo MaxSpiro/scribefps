f3766eb4fa8cd852f2252a5f7f554bd1cff19fd1
MaxSpiro <maxspiridonov04@gmail.com>
2022-08-31 13:04:47 -0700

    a

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 
 export const client = new sanityClient({
   projectId: import.meta.env.SANITY_PROJECT_ID,
-  dataset: "import.meta.env.SANITY_DATASET",
+  dataset: import.meta.env.SANITY_DATASET,
   apiVersion: '2022-08-24',
   useCdn: true,
 });
