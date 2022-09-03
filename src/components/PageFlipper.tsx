f3fc86d9c6988c39da07d5efef9f35ad9b97dfa8
MaxSpiro <maxspiridonov04@gmail.com>
2022-09-03 12:52:57 -0700

    working on responsiveness

--- src/components/PageFlipper.tsx
+++ src/components/PageFlipper.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface PageFlipperProps {
   volume: string;
@@ -16,10 +16,10 @@ const PageSizes: {
   SM: 480,
 };
 const PageFlipper = ({ volume, issue, pages, numPages }: PageFlipperProps) => {
-  const widthFromHeight = (height: number) => Math.floor((height * 8.5) / 11);
   const [page, setPage] = useState(0);
   const [height, setHeight] = useState<number>(PageSizes.LG);
-  console.log(Object.keys(PageSizes));
+  const width = Math.floor((height * 8.5) / 11);
+  const arrowSize = height / 30;
 
   const prevPage = () => {
     if (page > 0) {
@@ -33,19 +33,34 @@ const PageFlipper = ({ volume, issue, pages, numPages }: PageFlipperProps) => {
   };
   return (
     <>
-      <div className='grid w-full grid-cols-4 gap-2 px-6 py-4'>
+      <div className='my-2 grid w-full grid-cols-4 gap-2 px-6'>
         {Object.keys(PageSizes).map((pageSize) => {
           return (
-            <button className='rounded-xl bg-pink-600' onClick={() => setHeight(PageSizes[pageSize])}>
+            <button
+              className={`rounded-xl ${height === PageSizes[pageSize] ? 'bg-gray-400' : 'bg-gray-300'}`}
+              onClick={() => setHeight(PageSizes[pageSize])}
+            >
               {pageSize}
             </button>
           );
         })}
       </div>
-      <div className='flex items-center justify-center gap-6 p-6'>
-        <img onClick={prevPage} className='cursor-pointer' width={40} height={40} src='/icons/arrow-left.svg' />
-        {pages[page] && <img height={height} width={widthFromHeight(height)} src={pages[page].pageUrl} />}
-        <img onClick={nextPage} className='cursor-pointer' width={40} height={40} src='/icons/arrow-right.svg' />
+      <div className='flex items-center justify-center md:gap-6'>
+        <img
+          onClick={prevPage}
+          className='cursor-pointer'
+          width={arrowSize}
+          height={arrowSize}
+          src='/icons/arrow-left.svg'
+        />
+        {pages[page] && <img height={height} width={width} src={pages[page].pageUrl} />}
+        <img
+          onClick={nextPage}
+          className='cursor-pointer'
+          width={arrowSize}
+          height={arrowSize}
+          src='/icons/arrow-right.svg'
+        />
       </div>
     </>
   );
